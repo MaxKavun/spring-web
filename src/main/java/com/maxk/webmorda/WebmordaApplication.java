3c2696c0fad2c12f6aaa5cb3eee47675303d6933
Maksim Kavun <maksim.kavun@intexsoft.by>
2019-10-10 10:49:50 +0300

    Initializing spring app

---
+++ src/main/java/com/maxk/webmorda/WebmordaApplication.java
@@ -0,0 +1,13 @@
+package com.maxk.webmorda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebmordaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebmordaApplication.class, args);
+	}
+
+}
